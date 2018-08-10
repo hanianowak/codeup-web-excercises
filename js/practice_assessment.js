5ed6082ad9ee0db76abb269fda77ddcd283142a3
hanianowak <hania.nowak2018@gmail.com>
2018-08-09 22:10:36 -0500

    define functions - practise_assessment.js

--- js/practice_assessment.js
+++ js/practice_assessment.js
@@ -15,9 +15,20 @@ console.log(isTrue(true));
 
 // Define a function named isFalse that takes in a value and returns a true if and only if the provided input is equal to false in both type and value.
 //
-// isFalse(false)             // true
-// isFalse(true)              // false
-// isFalse(0)                 // false
+function isFalse(input) {
+    // if (input === false) {
+    //     return true;
+    // }
+    // else {
+    //     return false;
+    // }
+    //
+
+    return input === false;
+}
+console.log(isFalse(false));             // true
+console.log(isFalse(true));             // false
+console.log(isFalse(0));                 // false
 // isFalse(null)              // false
 // isFalse("")                // false
 // isFalse("Banana")          // false
@@ -26,7 +37,12 @@ console.log(isTrue(true));
 
 
 // Define a function named not that takes in any input and returns the boolean opposite of the provided input.
-//
+
+function not (input) {
+    return !input;
+}
+console.log(not(true));
+
 // not(false)                  // true
 // not(0)                      // true
 // not("")                     // true
@@ -41,7 +57,20 @@ console.log(isTrue(true));
 
 
 // Define a function named addOne that takes in a single input. If the input is a number or a numeric string, return the value plus one.
-//
+
+function addOne(input) {
+    if ((typeof(input) == 'number') || (typeof(parseFloat(input)) == "number")) {
+        input = parseFloat(input);
+        return input + 1;
+    }
+    else {
+        return NaN;
+    }
+}
+
+console.log(addOne(-2));
+console.log(addOne("2"));
+console.log(addOne("night"));
 // addOne(0)                    // 1
 // addOne(2)                    // 3
 // addOne(-5)                   // -4
@@ -57,6 +86,12 @@ console.log(isTrue(true));
 
 // Define a function named isEven that takes in a single input. If the input is an even number or a string containing an even number, return true. Any other input should return false for the output.
 //
+function isEven(input) {
+    return (input % 2 === 0) || (parseFloat(input) % 2 === 0);
+}
+console.log(isEven(2));
+console.log(isEven("hh"));
+
 // isEven(2)                   // true
 // isEven(-8)                  // true
 // isEven(0)                   // true
@@ -70,51 +105,81 @@ console.log(isTrue(true));
 
 // Define a function named isIdentical that takes in two input arguments. If each input is equal both in data type and in value, then return true. If the values are not the same data type or not the same value, return false.
 //
-// isIdentical(3, 3)                     // true
-// isIdentical(false, false)             // true
+function isIdentical(input1, input2) {
+    // if (input1 === input2) {
+    //     return true;
+    // }
+    // else {
+    //     return false;
+    // }
+    return input1 === input2;
+}
+
+console.log(isIdentical(3, 3));                     // true
+console.log(isIdentical(false, false));             // true
 // isIdentical("hello", "hello")         // true
 // isIdentical(3, 3.0)                   // true
 // isIdentical(undefined, undefined)     // true
-// isIdentical(2, "2")                   // false
+console.log(isIdentical(2, "2"));                   // false
 // isIdentical("javascript", "java")     // false
 
 
 
 // Define a function named isEqual that takes in two input arguments. If each argument is equal only in value, then return true. Otherwise return false.
 //
-// isEqual(3, "3")                       // true
-// isEqual("abc123", "abc123")           // true
-// isEqual(true, 1)                      // true
+function isEqual(input1, input2) {
+    if (input1 == input2) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+console.log(isEqual(3, "3"));                       // true
+console.log(isEqual("abc123", "abc123"));           // true
+console.log(isEqual(true, 1));                      // true
 // isEqual(0, false)                     // true
-// isEqual(4, -5)                        // false
+console.log(isEqual(4, -5));                        // false
 // isEqual("java", "javascript")         // false
 
 
 
 // Define a function named or that takes in two input arguments. The output returned should be the result of an or operation on both inputs.
 //
-// or(true, true)                    // true
-// or(true, false)                   // true
+function or(input1, input2) {
+    return input1 || input2;
+
+}
+console.log(or(true, true));                    // true
+console.log(or(true, false));                  // true
 // or(false, true)                   // true
 // or(false, false)                  // false
-// or("hello", "world")              // "hello" (this behavior is non-obvious, research more!)
+console.log(or("hello", "world"));              // "hello" (this behavior is non-obvious, research more!)
 
 
 
 // Define a function named and that takes in two input arguments and returns the result of a logical and operation of both inputs.
 //
-// and(true, true)                    // true
-// and(true, false)                   // false
+function and(input1, input2) {
+    return input1 && input2;
+}
+console.log(and(true, true));                    // true
+console.log(and(true, false));                   // false
 // and(false, true)                   // false
 // and(false, false)                  // false
-// and("hello", "world")              // "world" (this behavior is non-obvious, research more)
+console.log(and("hello", "world"));              // "world" (this behavior is non-obvious, research more)
 
 
 
 // Define a function named concat that takes in two input arguments. If both arguments are strings, then return the concatenated result. If two numbers are provided, then return the string concatenation of each set of numerals.
 //
-// concat("code", "up")                // "codeup"
-// concat("connect", 4)                // "connect4"
+function concat(input1, input2) {
+    input1 = '' + input1;
+    input2 = '' + input2;
+    return input1 + input2;
+}
+console.log(concat("code", "up"));                // "codeup"
+console.log(concat("connect", 4));                // "connect4"
 // concat("hello", "world")            // "helloworld"
-// concat(4, 2)                        // "42"
+console.log(concat(4, 2));                        // "42"
 // concat(true, true)                  // "truetrue"
