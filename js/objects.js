aa821830c5995f79c3cd0979a595b79c8c41aee2
hanianowak <hania.nowak2018@gmail.com>
2018-08-16 20:22:13 -0500

    update

--- js/objects.js
+++ js/objects.js
@@ -201,16 +201,14 @@ console.log(books);
      *   name and returns a book object with the properties described
      *   previously. Refactor your code that creates the books array to instead
      *   use your function.
-
-
-
-
-
      *
      *
      * - Create a function named `showBookInfo` that accepts a book object and
      *   outputs the information described above. Refactor your loop to use your
      *   `showBookInfo` function.
-     */
+     *
+**/
+    function createBook (title, autor) {
 
+    }
 })();
