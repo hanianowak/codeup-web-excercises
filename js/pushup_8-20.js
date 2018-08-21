9e6fbf6230620a3fd0e1609d67e5324eadb3b466
hanianowak <hania.nowak2018@gmail.com>
2018-08-21 09:28:27 -0500

    add pushups 8/21

--- js/pushup_8-20.js
+++ js/pushup_8-20.js
@@ -16,3 +16,15 @@ function exponentiate(x, y) {
 }
 console.log(exponentiate(2,3));
 
+
+// 8-21
+function factorial(input) {
+    var product1 = 1;
+    for (var i=input; i>0; i--){
+
+           product1 *= i;
+
+       }
+       return product1;
+}
+console.log(factorial(5));
