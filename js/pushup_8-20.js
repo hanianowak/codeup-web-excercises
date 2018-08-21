a854e438f2ef1c0263d67fa28664057c695e41a8
hanianowak <hania.nowak2018@gmail.com>
2018-08-21 11:07:48 -0500

    update dom-query-js.html

--- js/pushup_8-20.js
+++ js/pushup_8-20.js
@@ -20,11 +20,23 @@ console.log(exponentiate(2,3));
 // 8-21
 function factorial(input) {
     var product1 = 1;
-    for (var i=input; i>0; i--){
-
-           product1 *= i;
-
-       }
-       return product1;
+    for (var i = input; i > 0; i--) {
+        product1 *= i;
+    }
+    return product1;
 }
+
 console.log(factorial(5));
+
+
+function summation(x) {
+     var product2 = 0;
+     for (var i = x; i>0; i--){
+         product2 += i;
+     }
+     return product2;
+}
+console.log(summation(5));
+console.log(summation(2));
+console.log(summation(10));
+console.log(summation(4));
