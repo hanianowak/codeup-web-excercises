e799441dd1f42af6ec217ab5de6cada926ce1480
hanianowak <hania.nowak2018@gmail.com>
2018-08-23 14:25:54 -0500

    add google_maps_api.html

--- js/pushup_8-20.js
+++ js/pushup_8-20.js
@@ -40,3 +40,78 @@ console.log(summation(5));
 console.log(summation(2));
 console.log(summation(10));
 console.log(summation(4));
+
+
+// 8-22
+function capitalize (word) {
+      var letters = word.split('');
+      var newWord = letters.join;
+      // for(var i; i = letters[0]; i++){
+      //      newWord.push(letters[0].toUpperCase()) ;
+      // }
+      // console.log(newWord)   ;
+
+}
+ capitalize("hania") ;
+
+
+function capitalizaFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()
+}
+
+var word = "dOgGie";
+console.log(capitalizaFirstLetter("dOg"));
+console.log(capitalizaFirstLetter(word));
+
+
+
+
+// 8-23
+ var me = {
+     name: "Daniel Fryar",
+     hairColor: "brown",
+     eyeColor: "brown",
+     birthday: {
+         month: 8,
+         day: 29
+     },
+     juggle: function () {
+         console.log("Hej, look at me I'm juggling");
+     }
+ };
+
+ var wonderWoman = {
+     name: "Wonder Woman",
+     secretID: {
+         name: "Diana Prince",
+         occupation: "museum curator"
+     },
+     superPower: function(){
+         console.log("she can fly")
+     }
+ };
+
+ var superKid = {
+     name: "Agata",
+     power: function () {
+         console.log("I can run for 20 hours");
+
+     }
+ };
+
+
+ superKid.age = "8";
+ superKid.additionalPowers = function () {
+     console.log("fly!!!!!")
+ };
+
+ var students = [];
+ students.push(me);
+students.push(superKid);
+ // students.push(cole);
+ students[0].juggle();
+
+
+
+
+ console.log(students);
