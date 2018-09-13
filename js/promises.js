9ba5d2435e133cbe5b3d84845d68113fa995c477
hanianowak <hania.nowak2018@gmail.com>
2018-09-12 20:51:55 -0500

    added second function

--- js/promises.js
+++ js/promises.js
@@ -17,21 +17,19 @@ wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 // Create a function that accepts a github username, and returns a promise that resolves with the date of the last commit
 // that user made. Reference the github api documentation to achieve this.
 
-// // function getTheDateOfCommit(username) {
-// fetch('https://api.github.com/repos/hanianowak/codeup-web-exercises/events',
-//     {headers: {'Authorization': '42982d8744b829018aaa96f17921e43cfd1095eb'}})
-//     .then(response => response.json())
-//     .then(function (response) {
-//         return myObject = JSON.parse(response)
-//     })
-//     .then(function (response) {
-//         var date0fLast = myObject[0].created_at;
-//         console.log(date0fLast)
-//     })
-//     .catch(error => console.error(error));
-//
-// // getTheDateOfCommit();
+function getTheDateOfCommit(username) {
+    var dateOfCommit = fetch(`https://api.github.com/repos/${username}/codeup-web-exercises/events`,
+        {headers: {'Authorization': '42982d8744b829018aaa96f17921e43cfd1095eb'}})
+        .then(response => response.json())
+        .then(function (response) {
+            return response[0].created_at;
+        })
+        .catch(error => console.error(error));
+    return dateOfCommit;
+}
+var cos = getTheDateOfCommit('hanianowak');
 
+console.log(cos);
 
 
 
